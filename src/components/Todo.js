bfcbca49bc70098590320835ac764b1051def67f
UncleLion <unclelion@ukr.net>
2023-03-14 14:42:32 +0300

    new edit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -22,7 +22,7 @@ function Todo( {todos, completeTodo, removeTodo, updateTodo, raw,} ) {
         return <TodoForm edit={edit} onSubmit={submitUpdate} />;
     }
 
-  return todos.map((todo, index) => (
+  return todos.map((todo, index) => {
     <div 
         className={todo.isComplete ? 'todo-row complete':'todo-row'} 
         key={index}
@@ -42,7 +42,7 @@ function Todo( {todos, completeTodo, removeTodo, updateTodo, raw,} ) {
             />
         </div>
     </div>
-  ))
+    })
 };
 
 export default Todo
